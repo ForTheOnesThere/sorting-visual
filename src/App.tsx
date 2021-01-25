31fe409e34d24ce142dad86275bc40d153d27275
ForTheOnesThere <jamescockayne1@gmail.com>
2021-01-25 20:02:36 +0000

    Quick sorting now returns an animation list, rather than a sorted array. The result is currently very wrong but this is definitely the way forward.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,28 @@ import Bars from './components/Bars'
 import { populateArray, randomiseArray, quickSort } from './functions';
 
 
-
+const quickSortAnimate = (array: any, setToSort: Function, animate:any):void => {
+  animate = quickSort(array)
+  console.log(animate)
+  for (let x = 0; x<animate.length; x++){
+    let bar1 = document.querySelectorAll('.bar')[animate[x][0]]
+    let bar2 = document.querySelectorAll('.bar')[animate[x][1]]
+    setTimeout(()=>{
+      bar1.classList.toggle('highlight')
+      bar2.classList.toggle('highlight')
+      setTimeout(()=>{
+        let temp = bar1.getAttribute('height')
+        bar1.setAttribute('height',`${bar2.getAttribute('height')}vh`)
+        bar2.setAttribute('height', `${temp}vh`)
+        setTimeout(()=>{
+          bar1.classList.toggle('highlight')
+          bar2.classList.toggle('highlight')
+        }, 100*(x+1))
+      },100*(x+1))
+    }, 1000*(x+1))
+  }
+  return
+}
 
 
 function App() {
@@ -24,14 +45,13 @@ function App() {
   useEffect(() => {
     updateArray(arrayLength)
   },[arrayLength])
-//<button onClick={() => {setToSort(quickSort(toSort))}}>Sort</button>
 
   return (
     <div className="App">
       <h2>Move the slider to customise the array length!</h2>
       <input type="range" onChange={(event) => setArrayLength(Number(event.target.value))} id="slider" defaultValue="20" min="10" max="1000"/>
       <button onClick={() => {
-        quickSort(toSort, setToSort)
+        quickSortAnimate(toSort, setToSort, [])
       }}>Sort</button>
       <Bars toSort={toSort} />
     </div>
--- src/functions.tsx
+++ src/functions.tsx
@@ -24,33 +24,29 @@ const swap = (i: number, j: number, array: number[]): number[] => {
   return array
 }
 
-export const quickSort = (array: number[], setToSort: Function): void => {
+export const quickSort = (array: number[]): number[][] => {
 
-  if (!array.length) {return}
+  //if (!array.length) {return array}
 
   let pivot = array[array.length-1]
   let i = -1
+  let animate = []
 
   for (let j=0; j<array.length-1; j++){
     if (array[j] <= pivot) {
       i++
+      animate.push([i, j])
       swap(i, j, array)
     }
 }
 
   array.splice(i+1, 0, pivot)
   array.pop()
+  animate.push([i+1, array.length-1])
 
   let left = array.slice(0,i+1)
   let right = array.slice(i+2, array.length)
 
-
-  setTimeout(()=>{
-    setToSort([...array])
-    if (!left.length){quickSort(right, setToSort)}
-    else {quickSort(left, setToSort)}
-  }, 1000)
-
-  return
+  return animate
   //return quickSort(left).concat(pivot, quickSort(right))
 }
